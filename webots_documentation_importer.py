a0b41eeb25b69f7430ef7d6dba702741a587b9fb
fabienrohrer <fabien.rohrer@gmail.com>
2016-01-21 11:36:42 +0100

    Progress with the documentation importer script.

--- webots_documentation_importer.py
+++ webots_documentation_importer.py
@@ -5,18 +5,37 @@ import re
 import xml.etree.ElementTree as ET
 
 
-webotsDirectory = '../webots/'
-documentationInputDirectories = [webotsDirectory + 'src/doc/guide/', webotsDirectory + 'src/doc/reference/']
+def parsePara(root, outFile):
+    outFile.write(((root.attrib.get('name', root.text)) + '\n').encode('utf-8'))
 
-indent = 0
+def parseSection(root, outFile):
+    for child in root.getchildren():
+        if child.tag == 'title':
+            outFile.write('## ' + child.attrib.get('name', child.text) + '\n\n')
+        elif child.tag == 'para':
+            parsePara(child, outFile);
 
-def printRecur(root, outFile):
-    outFile.write((u' '*indent + u'%s: %s' % (root.tag.title(), root.attrib.get('name', root.text))).encode('utf-8'))
-    global indent
-    indent += 4
-    for elem in root.getchildren():
-        printRecur(elem, outFile)
-    indent -= 4
+def parseBook(root, outFile):
+    for child in root.getchildren():
+        if child.tag == 'bookinfo':
+            for subchild in child.getchildren():
+                if subchild.tag == 'title':
+                    outFile.write('# ' + subchild.attrib.get('name', subchild.text) + '\n\n')
+        elif child.tag == 'preface':
+            parseSection(child, outFile)
+
+def parseChapter(root, outFile):
+    pass
+
+def parseXmlElement(root, outFile):
+    #outFile.write((u' '*indent + u'%s: %s' % (root.tag.title(), root.attrib.get('name', root.text))).encode('utf-8'))
+    if root.tag == 'book':
+        parseBook(root, outFile)
+    elif root.tag == 'chapter':
+        parseChapter(root, outFile)
+    else:
+        for child in root.getchildren():
+            parseXmlElement(child, outFile)
 
 
 def parseXMLFile(filePath):
@@ -38,15 +57,19 @@ def parseXMLFile(filePath):
     root = ET.fromstring(content)
 
     outFile = open(outputFilePath, 'w')
-    printRecur(root, outFile)
+    parseXmlElement(root, outFile)
     outFile.close()
 
 
-for inputDirectory in documentationInputDirectories:
-    directoryName = os.path.basename(os.path.dirname(inputDirectory)) # guide or reference
-    print 'Directory name: ' + directoryName
+if __name__ == "__main__":
+    webotsDirectory = '../webots/'
+    documentationInputDirectories = [webotsDirectory + 'src/doc/guide/', webotsDirectory + 'src/doc/reference/']
 
-    intputFilePath = inputDirectory + directoryName + '.xml'
-    print 'Input file name: ' + intputFilePath
+    for inputDirectory in documentationInputDirectories:
+        directoryName = os.path.basename(os.path.dirname(inputDirectory)) # guide or reference
+        print 'Directory name: ' + directoryName
 
-    parseXMLFile(intputFilePath)
+        intputFilePath = inputDirectory + directoryName + '.xml'
+        print 'Input file name: ' + intputFilePath
+
+        parseXMLFile(intputFilePath)

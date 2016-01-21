1a929afe4154c6c8e7d91381c8685f39def90bf8
fabienrohrer <fabien.rohrer@gmail.com>
2016-01-21 15:34:20 +0100

    Progress with the importer

--- webots_documentation_importer.py
+++ webots_documentation_importer.py
@@ -2,16 +2,34 @@
 
 import os
 import re
+import textwrap
 import xml.etree.ElementTree as ET
 
-
 def parsePara(root, outFile):
-    outFile.write(((root.attrib.get('name', root.text)) + '\n').encode('utf-8'))
+    if root.text:
+        text = root.text.encode('utf-8')
+        content = text.split('\n')
+        text = ' '.join(map(lambda x: x.strip(), content)).strip()
+        content = textwrap.wrap(text, width=80)
+        for line in content:
+            outFile.write(line.strip() + '\n')
+        outFile.write('\n')
 
-def parseSection(root, outFile):
+def parseChapter(root, outFile):
     for child in root.getchildren():
-        if child.tag == 'title':
-            outFile.write('## ' + child.attrib.get('name', child.text) + '\n\n')
+        if child.tag == 'title' and child.attrib.get('name', child.text):
+            indent = 1
+            if root.tag == 'chapter':
+                indent = 2
+            elif root.tag == 'sect1':
+                indent = 3
+            elif root.tag == 'sect2':
+                indent = 4
+            elif root.tag == 'sect3':
+                indent = 5
+            outFile.write('#' * indent + ' ' + child.attrib.get('name', child.text) + '\n\n')
+        elif child.tag == 'sect1' or child.tag == 'sect2' or child.tag == 'sect3':
+            parseChapter(child, outFile);
         elif child.tag == 'para':
             parsePara(child, outFile);
 
@@ -22,20 +40,17 @@ def parseBook(root, outFile):
                 if subchild.tag == 'title':
                     outFile.write('# ' + subchild.attrib.get('name', subchild.text) + '\n\n')
         elif child.tag == 'preface':
-            parseSection(child, outFile)
+            parseChapter(child, outFile)
+        elif child.tag == 'require':
+            print 'require' + child.attrib.get('tag')
+            parseXMLFile(child.attrib.get('tag'))
 
-def parseChapter(root, outFile):
-    pass
-
-def parseXmlElement(root, outFile):
+def parseTopXmlElement(root, outFile):
     #outFile.write((u' '*indent + u'%s: %s' % (root.tag.title(), root.attrib.get('name', root.text))).encode('utf-8'))
     if root.tag == 'book':
         parseBook(root, outFile)
     elif root.tag == 'chapter':
         parseChapter(root, outFile)
-    else:
-        for child in root.getchildren():
-            parseXmlElement(child, outFile)
 
 
 def parseXMLFile(filePath):
@@ -52,12 +67,13 @@ def parseXMLFile(filePath):
       content = file.read()
     # xml.etree doesn't support the ENTITY - SYSTEM tags
     # => the following line are converting them to a custom <require> tag
-    content = re.sub(r'&(.*)_chapter;', r'<require tag="' + inputDirectory + r'\1.xml" />', content)
+    content = re.sub(r'&(.*)_chapter;', r'<require tag="' + inputDirectory + r'\1_chapter.xml" />', content)
+    content = re.sub(r'&(\w*);', r'\1', content) # TODO: improve this
 
     root = ET.fromstring(content)
 
     outFile = open(outputFilePath, 'w')
-    parseXmlElement(root, outFile)
+    parseTopXmlElement(root, outFile)
     outFile.close()
 
 

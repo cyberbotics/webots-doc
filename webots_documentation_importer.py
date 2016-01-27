b110eb1a136fb138f99d1f421345b997bb98131f
fabienrohrer <fabien.rohrer@gmail.com>
2016-01-27 08:56:17 +0100

    Migrated the misc parsing functions into a class, in order to facilitate the implementation (the global attriibutes were a pain to share between functions)

--- webots_documentation_importer.py
+++ webots_documentation_importer.py
@@ -13,178 +13,189 @@ def slugify(txt):
   output = re.sub(r'\W+', '-', output)
   return output.strip(' ').strip('-')
 
-def getTitle(el):
-  titleNodes = el.findall('title')
-  if len(titleNodes) <= 0:
-      raise Exception('Cannot find title')
-  return titleNodes[0].text
+class BookParser:
+    def __init__(self, webotsDirectoryPath, bookName):
+        self.bookName = bookName
+        self.root = None
+        # recreate the target directory if any
+        if os.path.exists(bookName):
+            shutil.rmtree(bookName)
+        os.makedirs(bookName)
+        # copy the png images
+        shutil.copytree(webotsDirectoryPath + 'doc/' + bookName + '/png', bookName + '/png')
 
-def parseText(txt):
-    if txt is None:
-        return ''
-    return txt.encode('utf-8')
+    def parseXMLFile(self, filePath):
+        print 'Parse XML file: "' + filePath + '"'
+        baseName = os.path.basename(filePath)
+        inputDirectoryPath = os.path.dirname(filePath)
 
-def parsePara(root, outFile):
-    # para included tags:
-    #     bold, command, computeroutput, email, emphasis, filename, function,
-    #     guibutton, guilabel, guimenu, guimenuitem, guisubmenu, hlink,
-    #     inlinegraphic, keycap, math, parameter, programlisting, trademark
-    #     ulink, xref
-    text = ''
-    if root.text:
-        text += parseText(root.text)
-    for child in root.getchildren():
-        if child.tag == 'bold' and child.text:
-            text += '**' + parseText(child.text) + '**'
-        elif child.tag == 'emphasis' and child.text:
-            text += '*' + parseText(child.text) + '*'
-        elif child.tag == 'filename' and child.text:
-            text += '"' + parseText(child.text) + '"'
-        elif child.text:
-            text += '`' + parseText(child.text) + '`'
-        text += parseText(child.tail)
-    text += parseText(root.tail)
+        with open(filePath, 'r') as file:
+          content = file.read()
 
-    content = text.split('\n')
-    text = ' '.join(map(lambda x: x.strip(), content)).strip()
-    content = textwrap.wrap(text, width=80)
-    for line in content:
-        outFile.write(line.strip() + '\n')
-    outFile.write('\n')
-
-def parseProgramListing(root, outFile):
-    outFile.write('\n```')
-    lang = root.attrib.get('lang')
-    if lang is not None:
-        if 'c' in lang:
-            outFile.write(' c')
-        elif 'C' in lang:
-            outFile.write(' c++')
-        elif 'J' in lang:
-            outFile.write(' java')
-        elif 'P' in lang:
-            outFile.write(' python')
-        elif 'M' in lang:
-            outFile.write(' matlab')
-    outFile.write('\n')
-    outFile.write(parseText(root.text) + '\n')
-    outFile.write('```\n\n')
-
-def parseFigure(root, outFile):
-    title = ''
-    fileref = ''
-    for child in root.getchildren():
-        if child.tag == 'title':
-            title = child.text
-        elif child.tag  == 'graphic':
-            fileref = child.attrib.get('fileref')
-    if title is not None and len(title) > 0 and fileref is not None and len(fileref) > 0:
-        outFile.write('![%s](%s)\n**%s**\n\n' % (title, fileref, title))
-
-def parseChapter(root, outFile, outputDirectoryPath):
-    for child in root.getchildren():
-        if child.tag == 'title' and child.attrib.get('name', child.text):
-            indent = 0
-            if root.tag == 'chapter':
-                indent = 1
-            elif root.tag == 'sect1' or root.tag == 'preface':
-                indent = 2
-            elif root.tag == 'sect2':
-                indent = 3
-            elif root.tag == 'sect3':
-                indent = 4
+        # deal with preprocessor instructions
+        while True:
+            mo = re.search(r'(&\w*;)', content)
+            if not mo:
+                break
+            tag = mo.group(0)[1:-1] # remove the first and last characters
+            output = ''
+            if tag.endswith('_chapter'):
+                with open(inputDirectoryPath + "/" + tag + '.xml', 'r') as includeFile:
+                    output = includeFile.read()
             else:
-                raise Exception('Unsupported type: ' + root.tag)
-            outFile.write('#' * indent + ' ' + child.attrib.get('name', child.text) + '\n\n')
-        elif child.tag == 'sect1':
-            fileName = outputDirectoryPath + slugify(getTitle(child)) + ".md"
-            print 'Generating ' + fileName
-            if os.path.exists(fileName):
-                raise Exception('sec1: "' + fileName + '" is existing')
-            outFile = open(fileName, 'w')
-            parseChapter(child, outFile, outputDirectoryPath)
-            outFile.close()
-        elif child.tag == 'sect2' or child.tag == 'sect3':
-            parseChapter(child, outFile, outputDirectoryPath)
-        elif child.tag == 'para':
-            parsePara(child, outFile)
-        elif child.tag == 'programlisting':
-            parseProgramListing(child, outFile)
-        elif child.tag == 'figure':
-            parseFigure(child, outFile)
+                # TODO: improve this
+                output = tag
+            content = content[:mo.start()] + output + content[mo.end():]
 
-def parseBook(root, outFile, outputDirectoryPath):
-    for child in root.getchildren():
-        if child.tag == 'bookinfo':
-            for subchild in child.getchildren():
-                if subchild.tag == 'title':
-                    outFile.write('# ' + subchild.attrib.get('name', subchild.text) + '\n\n')
-        elif child.tag == 'preface':
-            fileName = outputDirectoryPath + slugify(getTitle(child)) + ".md"
-            print 'Generating ' + fileName
-            if os.path.exists(fileName):
-                raise Exception('preface: "' + fileName + '" is existing')
-            outFile = open(fileName, 'w')
-            parseChapter(child, outFile, outputDirectoryPath)
-            outFile.close()
-        elif child.tag == 'chapter':
-            fileName = outputDirectoryPath + slugify(getTitle(child)) + ".md"
-            print 'Generating ' + fileName
-            if os.path.exists(fileName):
-                raise Exception('chapter: "' + fileName + '" is existing')
-            outFile = open(fileName, 'w')
-            parseChapter(child, outFile, outputDirectoryPath)
-            outFile.close()
+        self.root = ET.fromstring(content)
 
-def parseXMLFile(filePath):
-    print 'Parse XML file: "' + filePath + '"'
-    baseName = os.path.basename(filePath)
-    inputDirectoryPath = os.path.dirname(filePath)
-    outputDirectoryPath = os.path.basename(inputDirectoryPath) + '/'
-    outputFilePath = outputDirectoryPath + baseName.replace('.xml', '.md')
-    print 'Output file: ' + outputFilePath
+    def export(self):
+        if self.root is None:
+            raise Exception('Cannot export book')
 
-    with open(filePath, 'r') as file:
-      content = file.read()
+        self.outputDirectoryPath = self.bookName + '/'
 
-    # deal with preprocessor instructions
-    while True:
-        mo = re.search(r'(&\w*;)', content)
-        if not mo:
-            break
-        tag = mo.group(0)[1:-1] # remove the first and last characters
-        output = ''
-        if tag.endswith('_chapter'):
-            with open(inputDirectoryPath + "/" + tag + '.xml', 'r') as includeFile:
-                output = includeFile.read()
-        else:
-            # TODO: improve this
-            output = tag
-        content = content[:mo.start()] + output + content[mo.end():]
+        outputFilePath = self.outputDirectoryPath + self.bookName + '.md'
+        print 'Generating file: ' + outputFilePath
+        outFile = open(outputFilePath, 'w')
+        self.parseBook(self.root, outFile)
+        outFile.close()
 
-    root = ET.fromstring(content)
+    def getTitle(self, el):
+      titleNodes = el.findall('title')
+      if len(titleNodes) <= 0:
+          raise Exception('Cannot find title')
+      return titleNodes[0].text
 
-    outFile = open(outputFilePath, 'w')
-    parseBook(root, outFile, outputDirectoryPath)
-    outFile.close()
+    def parseText(self, txt):
+        if txt is None:
+            return ''
+        return txt.encode('utf-8')
+
+    def parsePara(self, node, outFile):
+        # para included tags:
+        #     bold, command, computeroutput, email, emphasis, filename, function,
+        #     guibutton, guilabel, guimenu, guimenuitem, guisubmenu, hlink,
+        #     inlinegraphic, keycap, math, parameter, programlisting, trademark
+        #     ulink, xref
+        text = ''
+        if node.text:
+            text += self.parseText(node.text)
+        for child in node.getchildren():
+            if child.tag == 'bold' and child.text:
+                text += '**' + self.parseText(child.text) + '**'
+            elif child.tag == 'emphasis' and child.text:
+                text += '*' + self.parseText(child.text) + '*'
+            elif child.tag == 'filename' and child.text:
+                text += '"' + self.parseText(child.text) + '"'
+            elif child.text:
+                text += '`' + self.parseText(child.text) + '`'
+            text += self.parseText(child.tail)
+        text += self.parseText(node.tail)
+
+        content = text.split('\n')
+        text = ' '.join(map(lambda x: x.strip(), content)).strip()
+        content = textwrap.wrap(text, width=80)
+        for line in content:
+            outFile.write(line.strip() + '\n')
+        outFile.write('\n')
+
+    def parseProgramListing(self, node, outFile):
+        outFile.write('\n```')
+        lang = node.attrib.get('lang')
+        if lang is not None:
+            if 'c' in lang:
+                outFile.write(' c')
+            elif 'C' in lang:
+                outFile.write(' c++')
+            elif 'J' in lang:
+                outFile.write(' java')
+            elif 'P' in lang:
+                outFile.write(' python')
+            elif 'M' in lang:
+                outFile.write(' matlab')
+        outFile.write('\n')
+        outFile.write(self.parseText(node.text) + '\n')
+        outFile.write('```\n\n')
+
+    def parseFigure(self, node, outFile):
+        title = ''
+        fileref = ''
+        for child in node.getchildren():
+            if child.tag == 'title':
+                title = child.text
+            elif child.tag  == 'graphic':
+                fileref = child.attrib.get('fileref')
+        if title is not None and len(title) > 0 and fileref is not None and len(fileref) > 0:
+            outFile.write('![%s](%s)\n**%s**\n\n' % (title, fileref, title))
+
+    def parseChapter(self, node, outFile):
+        for child in node.getchildren():
+            if child.tag == 'title' and child.attrib.get('name', child.text):
+                indent = 0
+                if node.tag == 'chapter':
+                    indent = 1
+                elif node.tag == 'sect1' or node.tag == 'preface':
+                    indent = 2
+                elif node.tag == 'sect2':
+                    indent = 3
+                elif node.tag == 'sect3':
+                    indent = 4
+                else:
+                    raise Exception('Unsupported type: ' + node.tag)
+                outFile.write('#' * indent + ' ' + child.attrib.get('name', child.text) + '\n\n')
+            elif child.tag == 'sect1':
+                fileName = self.outputDirectoryPath + slugify(self.getTitle(child)) + ".md"
+                print 'Generating ' + fileName
+                if os.path.exists(fileName):
+                    raise Exception('sec1: "' + fileName + '" is existing')
+                outFile = open(fileName, 'w')
+                self.parseChapter(child, outFile)
+                outFile.close()
+            elif child.tag == 'sect2' or child.tag == 'sect3':
+                self.parseChapter(child, outFile)
+            elif child.tag == 'para':
+                self.parsePara(child, outFile)
+            elif child.tag == 'programlisting':
+                self.parseProgramListing(child, outFile)
+            elif child.tag == 'figure':
+                self.parseFigure(child, outFile)
+
+    def parseBook(self, node, outFile):
+        for child in node.getchildren():
+            if child.tag == 'bookinfo':
+                for subchild in child.getchildren():
+                    if subchild.tag == 'title':
+                        outFile.write('# ' + subchild.attrib.get('name', subchild.text) + '\n\n')
+            elif child.tag == 'preface':
+                fileName = self.outputDirectoryPath + slugify(self.getTitle(child)) + ".md"
+                print 'Generating ' + fileName
+                if os.path.exists(fileName):
+                    raise Exception('preface: "' + fileName + '" is existing')
+                outFile = open(fileName, 'w')
+                self.parseChapter(child, outFile)
+                outFile.close()
+            elif child.tag == 'chapter':
+                fileName = self.outputDirectoryPath + slugify(self.getTitle(child)) + ".md"
+                print 'Generating ' + fileName
+                if os.path.exists(fileName):
+                    raise Exception('chapter: "' + fileName + '" is existing')
+                outFile = open(fileName, 'w')
+                self.parseChapter(child, outFile)
+                outFile.close()
 
 
 if __name__ == "__main__":
-    webotsDirectory = '../webots/'
-    documentationInputDirectories = [webotsDirectory + 'src/doc/guide/', webotsDirectory + 'src/doc/reference/']
+    webotsDirectoryPath = '../webots/'
+    documentationInputDirectoriesPaths = [webotsDirectoryPath + 'src/doc/guide/', webotsDirectoryPath + 'src/doc/reference/']
 
-    for inputDirectory in documentationInputDirectories:
-        directoryName = os.path.basename(os.path.dirname(inputDirectory)) # guide or reference
+    for inputDirectoryPath in documentationInputDirectoriesPaths:
+        directoryName = os.path.basename(os.path.dirname(inputDirectoryPath)) # guide or reference
         print 'Directory name: ' + directoryName
 
-        intputFilePath = inputDirectory + directoryName + '.xml'
+        intputFilePath = inputDirectoryPath + directoryName + '.xml'
         print 'Input file name: ' + intputFilePath
 
-        # recreate the target directory if any
-        if os.path.exists(directoryName):
-            shutil.rmtree(directoryName)
-        os.makedirs(directoryName)
-        # copy the png images
-        shutil.copytree(webotsDirectory + 'doc/' + directoryName + '/png', directoryName + '/png')
-
-        parseXMLFile(intputFilePath)
+        bookParser = BookParser(webotsDirectoryPath, directoryName)
+        bookParser.parseXMLFile(intputFilePath)
+        bookParser.export()

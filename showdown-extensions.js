1587d179ee60b2f97d8ed52a171ea90cefe90cdc
fabienrohrer <fabien.rohrer@gmail.com>
2016-02-04 17:38:25 +0100

    Added a showdown extension to see the figures.

---
+++ showdown-extensions.js
@@ -0,0 +1,23 @@
+showdown.extension("wbFigure", function() {
+    'use strict';
+
+    return [
+        {
+            type: "lang",
+            filter: function(text, converter, options) {
+                text = text.replace(/%figure\s+"([^]+?)"([^]+?)%end/gi, function(match, title, content) {
+                    var foo = converter.makeHtml(content);
+                    return "<figure>" + foo + "<figcaption>" + title + "</figcaption></figure>";
+                });
+                return text;
+            }
+        },
+        {
+            type: "html",
+            regex: /<figure><p><img([^]+?)<\/p><figcaption>/gi,
+            replace: function (match, content) {
+                return "<figure><img" + content + "<figcaption>";
+            }
+        }
+    ]
+});

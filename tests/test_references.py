da6c5d2e9fdafa0fb4e80ba76ae4cdc100f508bf
fabienrohrer <fabien.rohrer@gmail.com>
2016-02-29 09:56:51 +0100

    Optimized the anchor parsing

--- tests/test_references.py
+++ tests/test_references.py
@@ -13,7 +13,7 @@ def slugify(txt):
     output = re.sub(r'<[^>]+>', '', output)
     output = output.replace('+', 'p')
     output = re.sub(r'[\(\):`]', '', output)
-    output = re.sub(r'\W+', '-', output)
+    output = re.sub(r'[\W-]+', '-', output)
     output = re.sub(r'^-*', '', output)
     output = re.sub(r'-*$', '', output)
     return output.strip(' ').strip('-')
@@ -21,6 +21,31 @@ def slugify(txt):
 
 class TestReferences(unittest.TestCase):
     """Unit test of the references."""
+
+    def setUp(self):
+        """Setup."""
+        books = Books()
+        self.anchors = {}
+        for book in books.books:
+            for md_path in book.md_paths:
+                anchors = []
+                with open(md_path) as f:
+                    for line in f:
+                        if '<a' in line and 'name=' in line:
+                            for m in re.finditer(
+                                    r'<a[^>]*name\s*=\s*"(.*)"[^>]*>',
+                                    line.strip()):
+                                anchors.append(m.group(1))
+                        if line.startswith('#'):
+                            title = re.sub(r'^#*', '', line)
+                            anchors.append(slugify(title))
+                        elif line.startswith('%figure'):
+                            title = line.replace('%figure', '')
+                            anchors.append(slugify(title))
+                        elif line.startswith('%api'):
+                            title = line.replace('%api', '')
+                            anchors.append(slugify(title))
+                self.anchors[md_path] = anchors
 
     def test_references_are_valid(self):
         """Test that the MD files are pointing on valid URLs."""
@@ -61,28 +86,8 @@ class TestReferences(unittest.TestCase):
                     # 4. Anchor
                     if len(ref) > 1:
                         anchor = ref[1]
-                        with open(file_path) as f:
-                            found = False
-                            for line in f:
-                                if anchor in line:
-                                    found = True
-                                    continue
-                                elif line.startswith('#'):
-                                    title = re.sub(r'^#*', '', line)
-                                    if anchor == slugify(title):
-                                        found = True
-                                        continue
-                                elif line.startswith('%figure'):
-                                    title = line.replace('%figure', '')
-                                    if anchor == slugify(title):
-                                        found = True
-                                        continue
-                                elif line.startswith('%api'):
-                                    title = line.replace('%api', '')
-                                    if anchor == slugify(title):
-                                        found = True
-                                        continue
+                        found = anchor in self.anchors[file_path]
                         self.assertTrue(
-                            found, msg='%s -> %s:%s not found' %
+                            found, msg='%s: %s#%s not found' %
                             (md_path, file_path, anchor)
                         )

0f2001d43753c7e0e006432443021fa4fbe3e775
fabienrohrer <fabien.rohrer@gmail.com>
2016-03-09 15:55:18 +0100

    Improved portability of the script.

--- local_exporter.py
+++ local_exporter.py
@@ -18,18 +18,20 @@ def download(url, target_file_path):
 
 
 if __name__ == '__main__':
-    with open('index.html', 'r') as file:
+    script_directory = os.path.dirname(os.path.realpath(__file__)) + os.sep
+
+    with open(script_directory + 'index.html', 'r') as file:
         content = file.readlines()
 
     reg = r'"https://www\.cyberbotics\.com/([^"]*)"'
 
-    with open('local.html', 'w') as file:
+    with open(script_directory + 'local.html', 'w') as file:
         for line in content:
             m = re.search(reg, line)
             if m:
                 line = re.sub(reg, r'"dependencies/\1"', line)
                 relative_path = m.group(1)
-                target_path = 'dependencies/' + m.group(1)
+                target_path = script_directory + 'dependencies' + os.sep + m.group(1)
                 target_directory = os.path.dirname(target_path)
                 if not os.path.exists(target_directory):
                     os.makedirs(target_directory)

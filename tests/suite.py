048d666d160bb1ed5ea79946f17d39de9ab2d12a
fabienrohrer <fabien.rohrer@gmail.com>
2016-03-09 16:40:07 +0100

    Added a script to launch all the tests.

---
+++ tests/suite.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Run all the tests."""
+
+import unittest
+import sys
+
+
+if __name__ == "__main__":
+    logfile = None
+    loader = unittest.TestLoader()
+    tests = loader.discover('.')
+    testRunner = None
+    if len(sys.argv) > 1:
+        logfile = open(sys.argv[1], "w")
+        testRunner = unittest.runner.TextTestRunner(logfile)
+    else:
+        testRunner = unittest.runner.TextTestRunner()
+    testRunner.run(tests)
+    if logfile is not None:
+        logfile.close()

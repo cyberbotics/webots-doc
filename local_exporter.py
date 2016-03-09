9f7247d98eea9d72ac7a241d9f20c2d9b213528f
fabienrohrer <fabien.rohrer@gmail.com>
2016-03-09 15:52:20 +0100

    Implemented the local doc generator

--- local_exporter.py
+++ local_exporter.py
@@ -1,15 +1,40 @@
+#!/usr/bin/env python
+
 """Copy 'index.html' to 'local.html' and get JS dependencies locally."""
 
+import os
 import re
+import urllib2
+
+
+def download(url, target_file_path):
+    """Download URL to file."""
+    print 'Download "%s" to "%s"' % (url, target_file_path)
+    response = urllib2.urlopen(url, timeout=5)
+    content = response.read()
+    f = open(target_file_path, 'w')
+    f.write(content)
+    f.close()
 
 
 if __name__ == '__main__':
     with open('index.html', 'r') as file:
-        content = file.read()
+        content = file.readlines()
+
+    reg = r'"https://www\.cyberbotics\.com/([^"]*)"'
 
     with open('local.html', 'w') as file:
-        content = re.sub(
-            r'"https://www\.cyberbotics\.com/([^"]*)"',
-            r'"\1"', content
-        )
-        file.write(content)
+        for line in content:
+            m = re.search(reg, line)
+            if m:
+                line = re.sub(reg, r'"dependencies/\1"', line)
+                relative_path = m.group(1)
+                target_path = 'dependencies/' + m.group(1)
+                target_directory = os.path.dirname(target_path)
+                if not os.path.exists(target_directory):
+                    os.makedirs(target_directory)
+                download(
+                    'https://www.cyberbotics.com/' + relative_path,
+                    target_path
+                )
+            file.write(line)

bd33184bfe56901b9635f50bf6190988939f5a57
fabienrohrer <fabien.rohrer@gmail.com>
2016-07-18 17:34:50 +0200

    Restored urllib2 and increased timeout

--- local_exporter.py
+++ local_exporter.py
@@ -4,7 +4,7 @@
 
 import os
 import re
-import urllib
+import urllib2
 
 dependencies = [
     'highlight/9.5.0/default.min.css',
@@ -39,7 +39,7 @@ def download(url, target_file_path):
     """Download URL to file."""
     print 'Download "%s" to "%s"' % (url, target_file_path)
 
-    response = urllib.urlopen(url)
+    response = urllib2.urlopen(url,timeout=10)
     content = response.read()
 
     target_directory = os.path.dirname(target_file_path)

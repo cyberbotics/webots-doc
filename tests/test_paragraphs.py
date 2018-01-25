617c57ecbcadaa7b5bd74f3fa5598b9d86db1e01
fabienrohrer <fabien.rohrer@gmail.com>
2018-01-25 14:35:40 +0100

    Finer extraction of paragraphs.

--- tests/test_paragraphs.py
+++ tests/test_paragraphs.py
@@ -15,15 +15,34 @@ class TestParagraphs(unittest.TestCase):
         books = Books()
         for book in books.books:
             for md_path in book.md_paths:
+                # Extract MD content.
                 with open(md_path) as f:
                     content = f.read()
+
+                # Remove annoying string sequences.
+                # - Headers.
+                content = re.sub(r'^#.*', '', content)
+                content = re.sub(r'\n#.*', '\n', content)
+                # - Multiline code sections.
+                content = re.sub(r'```.*```', '', content, flags=re.S)
+                # - Items.
+                content = re.sub(r'\n\s*-.*', '', content)
+                content = re.sub(r'\n\s*\d+\..*', '', content)
+                # - Showdown extensions.
+                content = re.sub(r'%figure.*%end', '', content, flags=re.S)
+                content = re.sub(r'%api.*%end', '', content, flags=re.S)
+
+                # Extract paragraphs (and notes)
                 for match in re.finditer(r'(?s)((?:[^\n][\n]?)+)', content):
                     paragraph = content[match.start():match.end() - 1]
+                    # - Arrays.
+                    if paragraph.startswith('| '):
+                        continue
                     self.paragraphs.append(paragraph)
+        # Debug: Uncomment to display all the acquired paragraphs.
         # for p in self.paragraphs:
-        #     print '@@@'
-        #     print paragraph
-        #     print '@@@'
+        #     print ('@@@')
+        #     print (p)
 
     def test_something(self):
         """TODO."""

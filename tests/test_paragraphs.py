33c098b298773385e529110cfee6a1deea460f80
fabienrohrer <fabien.rohrer@gmail.com>
2018-01-25 12:17:20 +0100

    Get MD paragraphs.

---
+++ tests/test_paragraphs.py
@@ -0,0 +1,30 @@
+"""Test module for the paragraphs."""
+
+import unittest
+import re
+
+from books import Books
+
+
+class TestParagraphs(unittest.TestCase):
+    """Unit test of the MD files."""
+
+    def setUp(self):
+        """Setup: get all the paragraphs."""
+        self.paragraphs = []
+        books = Books()
+        for book in books.books:
+            for md_path in book.md_paths:
+                with open(md_path) as f:
+                    content = f.read()
+                for match in re.finditer(r'(?s)((?:[^\n][\n]?)+)', content):
+                    paragraph = content[match.start():match.end() - 1]
+                    self.paragraphs.append(paragraph)
+        # for p in self.paragraphs:
+        #     print '@@@'
+        #     print paragraph
+        #     print '@@@'
+
+    def test_something(self):
+        """Test that the MD files are existing."""
+        pass

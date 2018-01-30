3f6650353e9c69055242247d098e95af91d10031
fabienrohrer <fabien.rohrer@gmail.com>
2018-01-30 15:30:54 +0100

    Fixed typo.

--- js/viewer.js
+++ js/viewer.js
@@ -117,7 +117,7 @@ function forgeUrl(page, anchor) {
   var newUrl = currentUrl;
   if (isCyberboticsUrl) {
     var i = location.href.indexOf('cyberbotics.com/doc');
-    newUrl = location.href.substr(0, i) + "cyberbotics.com/docphp/" + setup.book + "/" + page;
+    newUrl = location.href.substr(0, i) + "cyberbotics.com/doctest/" + setup.book + "/" + page;
     if (setup.branch != '' && setup.repository && setup.repository != "omichel")
       newUrl += "?version=" + setup.repository + ":" + setup.branch;
     else if (setup.branch != '')
@@ -473,7 +473,7 @@ function changeMenuSelection() {
           n = href.indexOf('#');
           if (n > -1)
               href = href.substring(0, n)
-          if (href.endsWith("/docphp/" + setup.book + "/" + setup.page))
+          if (href.endsWith("/doctest/" + setup.book + "/" + setup.page))
               selection = true;
           else
               selection = false;

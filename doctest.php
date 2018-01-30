97a4854ca879a2d7cc08e24453b55bd441cefbae
fabienrohrer <fabien.rohrer@gmail.com>
2018-01-30 15:49:52 +0100

    Migrated the showdown-extensions.js as well.

--- doctest.php
+++ doctest.php
@@ -55,7 +55,7 @@
     <script src='https://www.cyberbotics.com/highlight/9.5.0/highlight.min.js'></script>
     <script src='https://www.cyberbotics.com/showdown/1.3.0/showdown.min.js'></script>
     <script src='https://www.cyberbotics.com/showdown/1.3.0/showdown-youtube.min.js'></script>
-    <script src='https://www.cyberbotics.com/wwi/R2018a/showdown-extensions.js'></script>
+    <script src='$rawgiturl$branch/js/showdown-extensions.js'></script>
     <script src='$rawgiturl$branch/js/viewer.js'></script>
   ";
   include 'header.php';

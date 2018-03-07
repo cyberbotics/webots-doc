0fb230d53c0201c1aa679177b3d777cb7ae58eb5
Tom Norton <Thomas.Norton@cyberbotics.com>
2018-03-07 08:54:46 +0100

    syntax error

--- js/viewer.js
+++ js/viewer.js
@@ -408,7 +408,7 @@ function highlightCode(view) {
   for (var i = 0; i < supportedLanguages.length; i++) {
     var language = supportedLanguages[i];
     hljs.configure({languages: [ language ]});
-    var codes = document.querySelectorAll([class='.language-' + language]);
+    var codes = document.querySelectorAll("[class='.language-'" + "language]");
     for (var j = 0; j < codes.length; j++) {
       var code = codes[j];
       hljs.highlightBlock(code);

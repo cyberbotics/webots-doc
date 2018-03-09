df2efab274bce9540b98ec0375511c5dd567b201
DavidMansolino <David.Mansolino@cyberbotics.com>
2018-03-09 15:16:23 +0100

    Removed unrelated change.

--- tests/test_paragraphs.py
+++ tests/test_paragraphs.py
@@ -29,7 +29,6 @@ class TestParagraphs(unittest.TestCase):
                 # - Showdown extensions.
                 content = re.sub(r'%chart.+?(?=%end)%end', '\n', content, flags=re.S)
                 content = re.sub(r'%figure.+?(?=%end)%end', '\n', content, flags=re.S)
-                content = re.sub(r'%chart.+?(?=%end)%end', '\n', content, flags=re.S)
                 content = re.sub(r'%api.+?(?=%end)%end', '\n', content, flags=re.S)
                 # - Headers.
                 content = re.sub(r'^#.*', '\n', content)
